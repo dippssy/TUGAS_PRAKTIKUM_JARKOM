156bc2a4bcdf8addfe24da320c477663eaa49ecd
dippssy <difaauly@gmail.com>
2026-04-13 16:44:26 +0700

    Modul 7

---
+++ Modul7/tcp-server.py
@@ -0,0 +1,44 @@
+from socket import *
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_STREAM) #tcp
+
+#meng bind server
+serverSocket.bind(
+    ('', serverPort)
+)
+
+#server siap menerima koneksi
+serverSocket.listen(1)
+
+print("[SYSTEM] Server TCP siap digunakan")
+
+running = True
+while running:
+    #menerima koneksi dari client
+    connectionSocket, addr = serverSocket.accept() ##menangkap tuple
+
+    while True:
+        #pesan yang diterima = 101001
+        message = connectionSocket.recv(2048).decode()
+
+        if not message:
+            break
+        #cek apakah exit    
+        if message.lower() == "exit":
+            print("[SYSTEM] client ingin keluar")
+            running = False
+            break   
+
+        #mofid capslock
+        modifiedMessage = message.upper()
+        print("[SERVER] Pesan yang diterima: ", modifiedMessage)
+
+        #kirim balik ke client
+        connectionSocket.send(
+            modifiedMessage.encode()
+        )
+
+    connectionSocket.close()
+    
+serverSocket.close()        
