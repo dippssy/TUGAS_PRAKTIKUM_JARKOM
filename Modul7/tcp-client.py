59199a1a9758ad77b3323d0a40b6bad108eaa497
dippssy <difaauly@gmail.com>
2026-04-15 15:42:38 +0700

    tcp client done

--- Modul7/tcp-client.py
+++ Modul7/tcp-client.py
@@ -12,3 +12,27 @@ clientSocket.connect(
 )
 
 print("[SYSTEM] Masukkan Pesan")
+
+running = True
+while running:
+    #input
+    message = input(">> ")
+
+    #kirim ke server
+    #encode = abcdef = 1010100110
+    clientSocket.send(message.encode())
+    
+    #kalo exit = socket tutup
+    if message == "exit":
+        print("[SYSTEM] Keluar dari program")
+        running = False
+        break
+
+    #menerima pesan dari server
+    modifiedMessage = clientSocket.recv(2048) #abc = 01100110010101, diset max bytenya 2048
+    print("[SERVER] pesan: ", modifiedMessage.decode())
+
+#tutup socket yang tidak dipakai
+clientSocket.close()
+print("[SYSTEM] Socket ditutup")  
+
